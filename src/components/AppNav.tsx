b7f0c861658345d6d27eadb8f612d2e8d1a65678
karolina269 <karolinastec269@gmail.com>
2024-04-13 17:59:32 +0200

    nav dropdown item, animals voice logo font

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -5,42 +5,69 @@ import { IoClose, IoMenu } from "react-icons/io5";
 import "./AppNav.css";
 
 const AppNav = () => {
-    const [showMenu, setShowMenu] = useState<boolean>(false);
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-        console.log(showMenu)
-      };
-      const closeMenuOnMobile = () => {
-        if (window.innerWidth <= 1150) {
-          setShowMenu(false);
-        }
-      };
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    console.log(showMenu);
+  };
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
 
   return (
     <header className="header">
       <nav className="nav">
         <NavLink to="/" className="nav__logo">
-          <h1>Animal's Voice</h1>
+          <h1>
+            Animal's
+            <br />
+            Voice
+          </h1>
         </NavLink>
-        <div className={`nav__menu ${showMenu ? "show-menu" : ""}`} id="nav-menu">
+        <div
+          className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
           <ul className="nav__list">
             <li className="nav__item">
-              <NavLink to="/about" className="nav__link" onClick={closeMenuOnMobile}>
+              <NavLink
+                to="/about"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
                 O nas
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink to="/actions" className="nav__link" onClick={closeMenuOnMobile}>
+              <NavLink
+                to="/actions"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
                 Nasze działania
               </NavLink>
             </li>
-            <li className="nav__item">
-              {/* <NavLink to="/about-us" className="nav__link" onClick={closeMenuOnMobile}> */}
+            <li className="nav__item dropdown">
+              <button
+                className="nav__link dropdown-button"
+                onClick={closeMenuOnMobile}
+              >
                 Polecamy
-              {/* </NavLink> */}
+              </button>
+              <div className="dropdown-content">
+                <a className="nav__link" href="#">Link 1</a>
+                <a className="nav__link" href="#">Link 2</a>
+                <a className="nav__link" href="#">Link 3</a>
+              </div>
             </li>
             <li className="nav__item">
-              <NavLink to="/contact" className="nav__link" onClick={closeMenuOnMobile}>
+              <NavLink
+                to="/contact"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
                 Kontakt
               </NavLink>
             </li>
@@ -59,29 +86,3 @@ const AppNav = () => {
 
 export default AppNav;
 
-{
-  /* <FontAwesomeIcon icon={icon} className="menuBars" onClick={() => setMenuOpen(!menuOpen)} />
-        <CSSTransition
-          in={menuOpen}
-          nodeRef={nodeRef}
-          timeout={0}
-          classNames="mainNavList"
-          unmountOnExit
-          onEnter={() => setIcon(faXmark)}
-          onExit={() => setIcon(faBars)}>
-          <ul ref={nodeRef} className="mainNavList" onMouseLeave={() => setMenuOpen(false)}>
-              <li className="mainNavItem">
-                  o nas
-              </li>
-              <li className="mainNavItem">
-                  nasze działania
-              </li>
-              <li className="mainNavItem">
-                  polecamy
-              </li>
-              <li className="mainNavItem">
-                  kontakt
-              </li>
-          </ul>
-        </CSSTransition> */
-}
